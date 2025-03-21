c0ef1da73db003745861bb41dbac4cab8b6bdb6a
yamajunn <amiton1115@gmail.com>
2025-03-21 15:24:28 +0900

    損失関数を追加し、モデルの出力を改善するためのコードを更新

--- src/GAN/GAN.py
+++ src/GAN/GAN.py
@@ -4,6 +4,78 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.keras.models import load_model
 
+import tensorflow as tf
+import numpy as np
+
+def l1_loss(y_true, y_pred):
+    return tf.reduce_mean(tf.abs(y_true - y_pred))
+
+def l2_loss(y_true, y_pred):
+    return tf.reduce_mean(tf.square(y_true - y_pred))
+
+def edge_loss(y_true, y_pred):
+    sobel_true = tf.image.sobel_edges(y_true)
+    sobel_pred = tf.image.sobel_edges(y_pred)
+
+    # X方向のエッジ差分
+    edge_x_loss = tf.abs(sobel_true[..., 0] - sobel_pred[..., 0])
+    # Y方向のエッジ差分
+    edge_y_loss = tf.abs(sobel_true[..., 1] - sobel_pred[..., 1])
+
+    # 両方のエッジの差分の平均を損失として使う
+    return tf.reduce_mean(edge_x_loss + edge_y_loss)
+
+def mae_loss(y_true, y_pred):
+    return tf.reduce_mean(tf.abs(y_true - y_pred))
+
+def mse_loss(y_true, y_pred):
+    return tf.reduce_mean(tf.square(y_true - y_pred))
+
+def ssim_loss(y_true, y_pred):
+    # SSIMは[0, 1]範囲の画像に適用されるため、出力を[0, 1]に正規化
+    y_true = (y_true + 1.0) / 2.0  # RGBA画像などの場合、[-1, 1]の範囲から[0, 1]に変換
+    y_pred = (y_pred + 1.0) / 2.0  # 同様に出力を[0, 1]に正規化
+    
+    # SSIMを計算
+    return 1 - tf.reduce_mean(tf.image.ssim(y_true, y_pred, max_val=1.0))
+
+def laplacian_filter(image):
+    """RGBAの各チャンネルにラプラシアンフィルタを適用"""
+    laplacian_kernel = tf.constant([
+        [0,  1,  0],
+        [1, -4,  1],
+        [0,  1,  0]
+    ], dtype=tf.float32)
+    
+    laplacian_kernel = tf.reshape(laplacian_kernel, [3, 3, 1, 1])  # (高さ, 幅, 入力チャンネル, 出力チャンネル)
+    
+    # RGBAの各チャンネルに適用するためのフィルタを作成
+    filters = tf.tile(laplacian_kernel, [1, 1, 4, 1])  # (3, 3, 4, 4) に拡張
+
+    # 4次元テンソル (バッチ, 高さ, 幅, チャンネル) の形状を維持
+    image = tf.expand_dims(image, axis=0)  # バッチ次元を追加 (None, H, W, 4)
+    edges = tf.nn.conv2d(image, filters, strides=[1, 1, 1, 1], padding="SAME")
+
+    return tf.squeeze(edges)  # バッチ次元を削除
+
+def laplacian_loss(y_true, y_pred):
+    edge_true = laplacian_filter(y_true)
+    edge_pred = laplacian_filter(y_pred)
+
+    # L1 損失
+    loss = tf.reduce_mean(tf.abs(edge_true - edge_pred))
+    return loss
+
+def total_loss(y_true, y_pred):
+    loss_l1 = l1_loss(y_true, y_pred)
+    # loss_edge = edge_loss(y_true, y_pred)
+    # loss_mae = mae_loss(y_true, y_pred)
+    # loss_mse = mse_loss(y_true, y_pred)
+    # loss_ssim = ssim_loss(y_true, y_pred)
+    loss_laplacian = laplacian_loss(y_true, y_pred)
+
+    return loss_l1 + 0.2 * loss_laplacian
+
 # Loss functions
 def generator_loss(disc_generated_output, gen_output, target, lambda_l1=100):
     # Adversarial loss
@@ -12,9 +84,10 @@ def generator_loss(disc_generated_output, gen_output, target, lambda_l1=100):
     
     # L1 loss (pixel-wise difference)
     l1_loss = tf.reduce_mean(tf.abs(target - gen_output))
+    loss_ssim = ssim_loss(target, gen_output)
     
     # Total loss
-    total_loss = gan_loss + (lambda_l1 * l1_loss)
+    total_loss = gan_loss + (lambda_l1 * l1_loss) + loss_ssim
     
     return total_loss, gan_loss, l1_loss
 
@@ -53,7 +126,7 @@ input_image = tf.concat([missing, mask], axis=-1)
 input_image = tf.expand_dims(input_image, 0)
 
 # 保存された学習済みモデルを読み込む
-model = load_model("models/GAN/checkpoint/pixelart_inpainting_generator_epoch_210.h5", custom_objects={'generator_loss': generator_loss, "discriminator_loss": discriminator_loss})
+model = load_model("models/GAN/GAN_500.h5", custom_objects={'generator_loss': generator_loss, "discriminator_loss": discriminator_loss})
 # model = load_model("models/laplacian_loss.h5", custom_objects={"laplacian_loss": laplacian_loss})
 
 predicted_image = model.predict(input_image)[0]
